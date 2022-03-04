159936e1db7a478b717b369ff8a147a03bf7ccd9
chernandezba <chernandezba@hotmail.com>
2022-03-04 20:05:30 +0100

    fix url images

--- src/generate_readme_md.sh
+++ src/generate_readme_md.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+URLIMAGES_BASE="https://raw.githubusercontent.com/chernandezba/zesarux/main"
+
 cat README
 echo
 
@@ -48,96 +50,96 @@ __Some screenshots__
 
 ZX Desktop, running multitask windows, on Solarized Dark GUI Style, running ZX Spectrum OverScan demo
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_zxdesktop_multitask.png "ZX Desktop")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_zxdesktop_multitask.png "ZX Desktop")
 
 
 ZX Desktop, running demo ny17 from TSConf, showing new direct access buttons starting from ZEsarUX version 9.1
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_zxdesktop_buttons.png "ZX Desktop Buttons")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_zxdesktop_buttons.png "ZX Desktop Buttons")
 
 
 ZX Spectrum Overscan demo
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_overscan.jpg "Overscan demo")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_overscan.jpg "Overscan demo")
 
 
 ZX-81 Mazogs
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_mazogs.png "Mazogs")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_mazogs.png "Mazogs")
 
 
 Sinclair QL
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_ql.png "QL")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_ql.png "QL")
 
 
 Cambridge Z88
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_z88.png "Z88")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_z88.png "Z88")
 
 
 ZX Spectrum Sir Fred running on curses (text) driver
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_sirfred_curses.png "Sirfred curses")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_sirfred_curses.png "Sirfred curses")
 
 
 ZX Spectrum The Great Escape running on curses (text) driver + utf8 extensions
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_greatescape_curses.png "The Great Escape curses")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_greatescape_curses.png "The Great Escape curses")
 
 
 ZX81 Mazogs running on curses (text) driver + utf8 extensions
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_mazogs_curses.png "Mazogs curses")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_mazogs_curses.png "Mazogs curses")
 
 
 ZX-Uno
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_zxuno.png "ZX-Uno")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_zxuno.png "ZX-Uno")
 
 
 ZX-Evolution TSConf
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_tsconf.jpeg "ZX-Evolution TSConf")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_tsconf.jpeg "ZX-Evolution TSConf")
 
 
 ZX Spectrum Next
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_tbblue.png "TBBlue/ZX Spectrum Next")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_tbblue.png "TBBlue/ZX Spectrum Next")
 
 
 Prism 512
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_prism.png "Prism 512")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_prism.png "Prism 512")
 
 
 Chloe 280SE
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_chloe280se.png "Chloe 280SE")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_chloe280se.png "Chloe 280SE")
 
 
 CPC 464
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_cpc.png "CPC 464")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_cpc.png "CPC 464")
 
 
 MSX
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_msx.png "MSX")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_msx.png "MSX")
 
 
 Colecovision
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_coleco.png "Colecovision")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_coleco.png "Colecovision")
 
 
 Sega SG-1000
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_sg1000.png "Sega SG-1000")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_sg1000.png "Sega SG-1000")
 
 
 Sega Master System
 
-![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_sms.png "Sega Master System")
+![alt text](${URLIMAGES_BASE}/screenshots/screenshot_sms.png "Sega Master System")
 
 
 Do you want to know how ZEsarUX looked like in the past? See this:
