f271f070eca494655f9fa8081dc224c7f8f1533f
chernandezba <chernandezba@hotmail.com>
2022-04-23 12:53:48 +0200

    improve menu item

--- src/menu_items_settings.c
+++ src/menu_items_settings.c
@@ -3144,9 +3144,11 @@ void menu_settings_audio(MENU_ITEM_PARAMETERS)
 
 
 			if (!strcmp(audio_new_driver_name,"pcspeaker")) {
-				menu_add_item_menu_format(array_menu_settings_audio,MENU_OPCION_NORMAL,menu_pcspeaker_intensive_cpu,NULL,"[%c] Better PC Speaker (but more cpu usage)",(audiopcspeaker_intensive_cpu_usage ? 'X' : ' ' ));
-
-
+                menu_add_item_menu_en_es_ca(array_menu_settings_audio,MENU_OPCION_NORMAL,menu_pcspeaker_intensive_cpu,NULL,
+                    "Improved PC Speaker","PC Speaker mejorado","PC Speaker millorat");
+                menu_add_item_menu_prefijo_format(array_menu_settings_audio,"[%c] ",(audiopcspeaker_intensive_cpu_usage ? 'X' : ' ' ));
+                menu_add_item_menu_tooltip(array_menu_settings_audio,"Improved sound but uses more cpu");
+                menu_add_item_menu_ayuda(array_menu_settings_audio,"Improved sound but uses more cpu");
 			}				
 	
 
