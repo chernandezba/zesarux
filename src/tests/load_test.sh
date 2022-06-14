85dbbc18b91455086beb6a28381829119a4bf217
chernandezba <chernandezba@hotmail.com>
2022-06-14 18:15:10 +0200

    add load_tests

---
+++ src/tests/load_test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Script to test load but also print char traps for printing ascii
+
+TEMPFILE=`mktemp`
+
+MAQUINAS="ZX80 ZX81 16k 48k 128k P2 P2F P2S P2A40 P2A41 P2AS TS2068 Inves 48ks 128ks TK90X TK90XS TK95 Pentagon ZXUNO"
+
+for i in $MAQUINAS; do
+
+	echo "Machine $i"
+	if [ "$i" == "ZX81" ]; then
+		./zesarux --noconfigfile --vo stdout tests/printtrap_test.p --exit-after 5 --machine ZX81 --cpuspeed 400 > $TEMPFILE
+	elif [ "$i" == "ZX80" ]; then
+		./zesarux --noconfigfile --vo stdout tests/printtrap_test.zsf --exit-after 3 --machine ZX80 --cpuspeed 400 > $TEMPFILE
+	else
+		./zesarux --noconfigfile --vo stdout tests/printtrap_test.tap --hardware-debug-ports --exit-after 5 --machine $i --fastautoload > $TEMPFILE
+	fi
+
+	grep HOLA $TEMPFILE
+	if [ $? != 0 ]; then
+		echo "ERROR Ascii"
+	else
+		echo "OK Ascii"
+	fi
+
+	echo
+
+done
+
