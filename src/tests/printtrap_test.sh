399ffecf02fdef2ca2c92eae83c68c238fee0b6e
chernandezba <chernandezba@hotmail.com>
2022-06-14 18:10:27 +0200

    move tests

--- src/tests/printtrap_test.sh
+++ src/tests/printtrap_test.sh
@@ -10,11 +10,11 @@ for i in $MAQUINAS; do
 
 	echo "Machine $i"
 	if [ "$i" == "ZX81" ]; then
-		./zesarux --noconfigfile --chardetectcompatnum --vo stdout printtrap_test.p --exit-after 5 --machine ZX81 --cpuspeed 400 > $TEMPFILE
+		./zesarux --noconfigfile --chardetectcompatnum --vo stdout tests/printtrap_test.p --exit-after 5 --machine ZX81 --cpuspeed 400 > $TEMPFILE
 	elif [ "$i" == "ZX80" ]; then
-		./zesarux --noconfigfile --vo stdout printtrap_test.zsf --exit-after 3 --machine ZX80 --cpuspeed 400 > $TEMPFILE
+		./zesarux --noconfigfile --vo stdout tests/printtrap_test.zsf --exit-after 3 --machine ZX80 --cpuspeed 400 > $TEMPFILE
 	else
-		./zesarux --noconfigfile --chardetectcompatnum --vo stdout printtrap_test.tap --hardware-debug-ports --exit-after 5 --machine $i --cpuspeed 400 > $TEMPFILE
+		./zesarux --noconfigfile --chardetectcompatnum --vo stdout tests/printtrap_test.tap --hardware-debug-ports --exit-after 5 --machine $i --cpuspeed 400 > $TEMPFILE
 	fi
 
 	grep 1234 $TEMPFILE
