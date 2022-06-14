dd441b1fa41ce3d90b71b14ccc3ec767db0a1c52
chernandezba <chernandezba@hotmail.com>
2022-06-14 18:40:04 +0200

    move tests paths

--- src/tests/generate_random_files.sh
+++ src/tests/generate_random_files.sh
@@ -18,7 +18,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Script to generate random files
+# Script to generate random files in order to test file browser and check it does not crash 
 
 
 while true; do
