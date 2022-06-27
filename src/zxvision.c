a9a3c0a86c77134e6786f8cba5b65f5e2e6087e6
chernandezba <chernandezba@hotmail.com>
2022-06-27 19:19:35 +0200

    control icons can not be put on lower device icons

--- src/zxvision.c
+++ src/zxvision.c
@@ -293,6 +293,23 @@ void zxvision_set_configurable_icon_position(int icon,int x,int y)
         return;
     }
 
+    //Ver si en posicion de iconos superiores
+    int xinicio_botones,xfinal_botones,yinicio_botones;
+    menu_ext_desktop_lower_icons_get_geometry(NULL,NULL,NULL,&xinicio_botones,&xfinal_botones,&yinicio_botones);
+    //Posiciones menos el zoom
+    xinicio_botones /=zoom_x;
+    xfinal_botones /=zoom_x;
+    yinicio_botones /=zoom_y;
+
+    //Consideramos el tamanyo del icono (ZESARUX_ASCII_LOGO_ANCHO) para que no se pueda ubicar medio icono fuera de rango por ejemplo
+    xinicio_botones -=ZESARUX_ASCII_LOGO_ANCHO;
+    yinicio_botones -=ZESARUX_ASCII_LOGO_ANCHO;
+
+    if (y>=yinicio_botones && x>=xinicio_botones && x<=xfinal_botones) {
+        printf("Wanted to set Icon position %d,%d on lower icons position. Do not change\n",x,y);
+        return;        
+    }
+
     zxdesktop_configurable_icons_list[icon].pos_x=x;
     zxdesktop_configurable_icons_list[icon].pos_y=y;
 }
