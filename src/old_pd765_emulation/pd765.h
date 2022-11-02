415c48a7296ce1efa429e3f8c6e65d5ae50a42b8
chernandezba <chernandezba@hotmail.com>
2022-11-02 19:35:02 +0100

    add old pd765 files

---
+++ src/old_pd765_emulation/pd765.h
@@ -0,0 +1,38 @@
+/*
+    ZEsarUX  ZX Second-Emulator And Released for UniX 
+    Copyright (C) 2013 Cesar Hernandez Bano
+
+    This file is part of ZEsarUX.
+
+    ZEsarUX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef PD765_H
+#define PD765_H
+
+#include "cpu.h"
+
+extern z80_bit pd765_enabled;
+extern void pd765_enable(void);
+extern void pd765_disable(void);
+
+extern void pd765_motor_on(void);
+extern void pd765_motor_off(void);
+extern void pd765_write_command(z80_byte value);
+extern z80_byte pd765_read_command(void);
+extern z80_byte pd765_read_status_register(void);
+
+
+#endif
