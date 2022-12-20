e2f694c113b753527b75f3fd323446fef854bdf4
chernandezba <chernandezba@hotmail.com>
2022-12-20 23:52:54 +0100

    code beautify

--- src/zxvision.c
+++ src/zxvision.c
@@ -21045,52 +21045,52 @@ void zxvision_scanf_history_insert(char **textos_historial,char *texto)
 
     int i;
 
-        //liberar ultima posicion
-        //int ultima_posicion=ZXVISION_SCANF_HISTORY_MAX_LINES-1; //contando que al final hay el NULL
+    //liberar ultima posicion
+    //int ultima_posicion=ZXVISION_SCANF_HISTORY_MAX_LINES-1; //contando que al final hay el NULL
 
-        //debug
-        for (i=0;i<=lineas_historial;i++) {
-            debug_printf(VERBOSE_DEBUG,"zxvision_scanf_history_insert: Before insert. Position %d Pointer %p (%s)",
-                i,textos_historial[i],(textos_historial[i]!=NULL ? textos_historial[i] : "NULL"));
-        }
-
-
-        //Ejemplo ZXVISION_SCANF_HISTORY_MAX_LINES vale 3
-        //Tenemos "string 1", "string 2", NULL
-        //si lleno, lineas_historial==3-1=2
-        //memoria a liberar = textos_historial[2-1] = tetos_historial [1]="string 2"
+    //debug
+    for (i=0;i<=lineas_historial;i++) {
+        debug_printf(VERBOSE_PARANOID,"zxvision_scanf_history_insert: Before insert. Position %d Pointer %p (%s)",
+            i,textos_historial[i],(textos_historial[i]!=NULL ? textos_historial[i] : "NULL"));
+    }
 
 
-        if (lineas_historial==ZXVISION_SCANF_HISTORY_MAX_LINES-1) {        //Liberar posicion ultima
-            char *mem_to_free=textos_historial[lineas_historial-1];
-            debug_printf(VERBOSE_DEBUG,"zxvision_scanf_history_insert: History is full. Freeing last position: %p",mem_to_free);
-            free(mem_to_free);
-        }
-        else {
-            lineas_historial++; 
-        }
-
-        // hacer hueco desplazando todo hacia abajo. con el ejemplo de arriba, empezamos en 1
-        for (i=lineas_historial-1;i>=1;i--) {
-            char *puntero=textos_historial[i-1];
-            textos_historial[i]=puntero;
-        }
-
-        //Meter NULL del final
-        textos_historial[lineas_historial]=NULL;
-
-        //Meter primera posicion, asignando memoria para string
-        int total_bytes=strlen(texto)+1;
-        char *newstring=util_malloc(total_bytes,"Can not allocate buffer for history string");
-        strcpy(newstring,texto);
-        textos_historial[0]=newstring;
+    //Ejemplo ZXVISION_SCANF_HISTORY_MAX_LINES vale 3
+    //Tenemos "string 1", "string 2", NULL
+    //si lleno, lineas_historial==3-1=2
+    //memoria a liberar = textos_historial[2-1] = tetos_historial [1]="string 2"
 
 
-        //debug
-        for (i=0;i<=lineas_historial;i++) {
-            debug_printf(VERBOSE_DEBUG,"zxvision_scanf_history_insert: After insert. Position %d Pointer %p (%s)",
-                i,textos_historial[i],(textos_historial[i]!=NULL ? textos_historial[i] : "NULL"));
-        }    
+    if (lineas_historial==ZXVISION_SCANF_HISTORY_MAX_LINES-1) {        //Liberar posicion ultima
+        char *mem_to_free=textos_historial[lineas_historial-1];
+        debug_printf(VERBOSE_DEBUG,"zxvision_scanf_history_insert: History is full. Freeing last position: %p",mem_to_free);
+        free(mem_to_free);
+    }
+    else {
+        lineas_historial++; 
+    }
+
+    // hacer hueco desplazando todo hacia abajo. con el ejemplo de arriba, empezamos en 1
+    for (i=lineas_historial-1;i>=1;i--) {
+        char *puntero=textos_historial[i-1];
+        textos_historial[i]=puntero;
+    }
+
+    //Meter NULL del final
+    textos_historial[lineas_historial]=NULL;
+
+    //Meter primera posicion, asignando memoria para string
+    int total_bytes=strlen(texto)+1;
+    char *newstring=util_malloc(total_bytes,"Can not allocate buffer for history string");
+    strcpy(newstring,texto);
+    textos_historial[0]=newstring;
+
+
+    //debug
+    for (i=0;i<=lineas_historial;i++) {
+        debug_printf(VERBOSE_PARANOID,"zxvision_scanf_history_insert: After insert. Position %d Pointer %p (%s)",
+            i,textos_historial[i],(textos_historial[i]!=NULL ? textos_historial[i] : "NULL"));
+    }    
 }
 
 //funcion como scanf pero con lineas adicionales por debajo que permiten elegir valores historicos
