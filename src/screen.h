2c103c46faa59202b89c9342bfcd03605f0c4704
chernandezba <chernandezba@hotmail.com>
2023-02-25 23:15:34 +0100

    add pcw palette to colour palette list

--- src/screen.h
+++ src/screen.h
@@ -710,7 +710,7 @@ struct s_total_palette_colours {
 typedef struct s_total_palette_colours total_palette_colours;
 
 //Esto usado en menu display->ver paleta total
-#define TOTAL_PALETAS_COLORES 15
+#define TOTAL_PALETAS_COLORES 16
 
 extern total_palette_colours total_palette_colours_array[];
 
