1f246eae4655fc61287789209c7a38ba96d43ed9
chernandezba <chernandezba@hotmail.com>
2023-05-08 19:20:41 +0200

    run some checklist

--- src/tests/games_tests.sh
+++ src/tests/games_tests.sh
@@ -11,6 +11,12 @@ echo "Test Coleco"
 echo "Test CPC"
 ./zesarux $OPTIONS --machine cpc464 --zoom 1 --zxdesktop-width 512 extras/media/cpc/zynaps.cdt
 
+echo "Test CPC 6128"
+./zesarux $OPTIONS --machine cpc6128 --zoom 1 --zxdesktop-width 512 "../../../soft_retro/cpc/Prince of Persia (1990)(Broderbund Software).dsk"
+
+echo "Test PCW"
+./zesarux $OPTIONS --machine pcw8256 --zoom 1 --zxdesktop-width 512 extras/media/pcw/Livingstone_Supongo.dsk
+
 echo "Test MSX"
 ./zesarux $OPTIONS --machine msx1 ../../../soft_retro/msx/Road\ Fighter\ \(1985\)\(Konami\)\[a\]\[RC-730\].rom
 
@@ -27,7 +33,10 @@ echo "Test ZX81"
 ./zesarux $OPTIONS --machine zx81 extras/media/zx81/MAZOGS.P
 
 echo "Test TSConf"
-./zesarux $OPTIONS --machine cpc464 --zoom 1 --zxdesktop-width 512 extras/media/spectrum/tsconf/fast.spg
+./zesarux $OPTIONS --machine tsconf --zoom 1 --zxdesktop-width 512 extras/media/spectrum/tsconf/fast.spg
 
 echo "Test Inves"
 ./zesarux $OPTIONS --machine inves ../../../soft_retro/spectrum/LEMMIN48.TAP
+
+echo "Test QL"
+./zesarux $OPTIONS --machine ql --zoom 1 --zxdesktop-width 512 --ql-mdv1-root-dir "/Users/cesarhernandez/Documents/ZEsarUX/zesarux/src/my_soft/ql/mago_folder" --enable-ql-mdv-flp --ql-mdv1-read-only
