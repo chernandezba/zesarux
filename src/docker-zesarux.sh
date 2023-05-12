c8626023f1f3634e70206801d380bab1e5875f09
chernandezba <chernandezba@hotmail.com>
2023-05-12 13:58:08 +0200

    mc

--- src/docker-zesarux.sh
+++ src/docker-zesarux.sh
@@ -12,7 +12,7 @@ docker-build() {
 }
 
 help() {
-	echo "$0 [run|run-curses|run-xorg|codetests]"
+	echo "$0 [build|codetests|run|run-curses|run-mac-xorg|run-xorg]"
 }
 
 if [ $# == 0 ]; then
@@ -23,6 +23,10 @@ fi
 case $1 in
 	
 
+
+	build)
+		docker-build
+	;;
 
 	run)
 		docker-build
@@ -38,6 +42,15 @@ case $1 in
 		docker-build
 		docker run -it -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --user="$(id --user):$(id --group)" zesarux --disableshm --ao null
 	;;
+
+	run-mac-xorg)
+		docker-build
+		export HOSTNAME=`hostname`
+		xhost +
+		echo "Be sure that xquarz preference Allow connections from network clients is enabled"
+		docker run -it -e DISPLAY=${HOSTNAME}:0 -v /tmp/.X11-unix:/tmp/.X11-unix  zesarux --disableshm --ao null
+	;;
+
 
 	codetests)
 		docker-build
