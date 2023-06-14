480b0233503bce30c7f33e1963671c45ab8edc16
chernandezba <chernandezba@hotmail.com>
2023-06-14 19:00:18 +0200

    set permissions to all writable for disk images on install

--- src/generate_install_sh.sh
+++ src/generate_install_sh.sh
@@ -18,6 +18,11 @@ COMMONFILES="ACKNOWLEDGEMENTS LICENSE LICENSES_info licenses Changelog Cambios T
 
 # -f to force overwrite already existing share files which are set to 444
 cp -f -a \$COMMONFILES $INSTALLPREFIX/share/zesarux/
+
+#set permissions to all writable for disk images
+chmod 666 $INSTALLPREFIX/share/zesarux/zxuno.flash
+chmod 666 $INSTALLPREFIX/share/zesarux/tbblue.mmc
+
 cp zesarux $INSTALLPREFIX/bin/
 
 
