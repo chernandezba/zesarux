64eb9abaed0a205ee6c4ecbfa1a9a2149bd937a9
chernandezba <chernandezba@hotmail.com>
2023-06-14 19:02:48 +0200

    fix perms

--- src/generate_install_sh.sh
+++ src/generate_install_sh.sh
@@ -19,15 +19,15 @@ COMMONFILES="ACKNOWLEDGEMENTS LICENSE LICENSES_info licenses Changelog Cambios T
 # -f to force overwrite already existing share files which are set to 444
 cp -f -a \$COMMONFILES $INSTALLPREFIX/share/zesarux/
 
-#set permissions to all writable for disk images
-chmod 666 $INSTALLPREFIX/share/zesarux/zxuno.flash
-chmod 666 $INSTALLPREFIX/share/zesarux/tbblue.mmc
-
 cp zesarux $INSTALLPREFIX/bin/
 
 
 # Default permissions for files: read only
 find $INSTALLPREFIX/share/zesarux/ -type f -print0| xargs -0 chmod 444
+
+#set permissions to all writable for disk images
+chmod 666 $INSTALLPREFIX/share/zesarux/zxuno.flash
+chmod 666 $INSTALLPREFIX/share/zesarux/tbblue.mmc
 
 # Speech filters can be run
 chmod +x $INSTALLPREFIX/share/zesarux/speech_filters/*
