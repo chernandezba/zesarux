1f7810a422600909442278f470e8ee453b5b1037
chernandezba <chernandezba@hotmail.com>
2023-09-14 20:14:46 +0200

    mc

--- src/tests/simulate_zeng_online_players/master.sh
+++ src/tests/simulate_zeng_online_players/master.sh
@@ -9,10 +9,14 @@ echo "Creating room"
 CREATOR_PASS=`( sleep 1 ; echo "zo create-room 0 pruebas" ; sleep 1 )|telnet $REMOTESERVER 10000|grep "command>"|head -1|awk '{printf $2}'`
 echo "Creator pass: $CREATOR_PASS"
 
-echo "Getting snapshot from this ZEsarUX"
-SNAPSHOT_DATA=`( sleep 1 ; echo "zo get-snapshot $USER_PASS 0" ; sleep 1 )|telnet localhost 10000|grep "command>"|head -1|awk '{printf $2}'`
+while true; do
+	echo "Getting snapshot from this ZEsarUX"
+	SNAPSHOT_DATA=`( sleep 1 ; echo "get-snapshot" ; sleep 1 )|telnet localhost 10000|grep "command>"|head -1|awk '{printf $2}'`
 
-echo "Putting snapshot to remote"
-( sleep 1 ; echo "zo put-snapshot $CREATOR_PASS 0 $SNAPSHOT_DATA" ; sleep 1 )|telnet $REMOTESERVER 10000
+	echo "Snapshot data: $SNAPSHOT_DATA"
+
+	echo "Putting snapshot to remote"
+	( sleep 1 ; echo "zo put-snapshot $CREATOR_PASS 0 $SNAPSHOT_DATA" ; sleep 1 )|telnet $REMOTESERVER 10000
+done
 
 
--- src/tests/simulate_zeng_online_players/slave.sh
+++ src/tests/simulate_zeng_online_players/slave.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#this local ZEsarUX on port 10001
+#./zesarux --noconfigfile --enable-remoteprotocol --remoteprotocol-port 10001
+
+
 REMOTESERVER=51.83.33.13
 
 
@@ -8,12 +12,13 @@ USER_PASS=`( sleep 1 ; echo "zo join 0" ; sleep 1 )|telnet $REMOTESERVER 10000|g
 echo "User pass: $USER_PASS"
 
 
-SNAPSHOT_DATA="00112233445566778899AABBCCDDEEFF"
+while true; do
+	echo "Getting remote snapshot"
+	RETURNED_SNAPSHOT_DATA=`( sleep 1 ; echo "zo get-snapshot $USER_PASS 0" ; sleep 1 )|telnet $REMOTESERVER 10000|grep "command>"|head -1|awk '{printf $2}'`
 
-echo "Getting remote snapshot"
-RETURNED_SNAPSHOT_DATA=`( sleep 1 ; echo "zo get-snapshot $USER_PASS 0" ; sleep 1 )|telnet $REMOTESERVER 10000|grep "command>"|head -1|awk '{printf $2}'`
+	echo "Received snapshot: $RETURNED_SNAPSHOT_DATA"
 
-echo "Putting local snapshot"
-( sleep 1 ; echo "put-snapshot $SNAPSHOT_DATA" ; sleep 1 )|telnet localhost 10000
+	echo "Putting local snapshot"
+	( sleep 1 ; echo "put-snapshot $RETURNED_SNAPSHOT_DATA" ; sleep 1 )|telnet localhost 10001
 
-
+done
