61fa071de4de7f7402ab32c52fe9a55a73bf9f1c
chernandezba <chernandezba@hotmail.com>
2023-11-27 18:49:40 +0100

    add funny message

--- src/start.c
+++ src/start.c
@@ -476,7 +476,7 @@ void print_funny_message(void)
 	//printf ("random: %d\n",randomize_noise[0]);
 
 	//mensajes random de broma
-	#define MAX_RANDOM_FUNNY_MESSAGES 31
+	#define MAX_RANDOM_FUNNY_MESSAGES 32
 	char *random_funny_messajes[MAX_RANDOM_FUNNY_MESSAGES]={
 		"Detected SoundBlaster at A220 I5 D1 T2",
 		"DOS/4GW Protected Mode Run-time  Version 1.97",		//2
@@ -508,7 +508,8 @@ void print_funny_message(void)
         "Z80 panic: shut her down Scotty, she's sucking mud again", //28
         "Not enough memory to display the error m",
         "ERROR 1164 HOW IN THE HELL DID YOU GET HERE", //30
-        "Good afternoon, gentelman, I'm a HAL 9000 Computer"
+        "Good afternoon, gentelman, I'm a HAL 9000 Computer",
+        "Neo-Geo. MAX 330 MEGA - PRO GEAR SPEC"
 	};
 
 
