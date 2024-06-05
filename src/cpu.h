2116a5d38261fd2ea743ca9f2d1ed757334a5843
chernandezba <chernandezba@hotmail.com>
2024-06-05 18:55:52 +0200

    change version date. fix some changelog entries in Spanish

--- src/cpu.h
+++ src/cpu.h
@@ -54,8 +54,8 @@ Examples
 //Para obtener EMULATOR_NUMBER_VERSION se deberia usar siempre util_get_emulator_version_number
 #define EMULATOR_NUMBER_VERSION EMULATOR_VERSION
 
-#define EMULATOR_DATE "20 May 2024"
-#define EMULATOR_SHORT_DATE "20/05/2024"
+#define EMULATOR_DATE "06 June 2024"
+#define EMULATOR_SHORT_DATE "06/06/2024"
 #define EMULATOR_GAME_EDITION "David"
 #define EMULATOR_EDITION_NAME EMULATOR_GAME_EDITION " edition"
 #define ZESARUX_EXTRAS_HOST "github.com"
