1503a14d53be8d62c24e2a06c5a9f18f79a46b3c
chernandezba <chernandezba@hotmail.com>
2024-06-25 18:58:52 +0200

    add keyboard help image for cz1500 plus

--- src/menu/menu_bitmaps.c
+++ src/menu/menu_bitmaps.c
@@ -5061,23 +5061,23 @@ char *bitmap_button_ext_desktop_my_machine_cz1500_plus[EXT_DESKTOP_BUTTONS_ANCHO
 	"                          ",
 	"                          ",
 	"                          ",
-	"      x    x x x          ",
-	"      xx   x x x          ",	//10
-	"      x x  x x x          ",
-	"      x  x x x x          ",
-	"      x   xx x x          ",
-    "      x    x x xxxxx      ",
-	"                          ",
-	"                          ",
-	"                          ",
-	"                          ",
-	"                          ",
-	"                          ",    //20
-	"                          ",
-	"                          ",
-	"                          ",
-	"                          ",
-	"                          " 	 //25
+    "                          ",
+	"xxxxxxxxxxxxxxxxxxxxxxxxxx",	//10
+	"xxxxxxxxxxxxxxxxxxxxxxxxxx",
+	"xxrrrxxxxxxxxxxxxxxxwwwwxx",
+	"xxxxxxxxxxxxxxxxxxxxxxxxxx",
+	"xxxxxxxxxxxxxxxxxxxxxxxxxx",
+	"xxxxxxxxxxxxxxxxxxxxxxxxxx",
+	"xwwwwwwwwwwwwwwwwwwwwwwwwx",
+	"xwxxwxxwxxwxxwxxwxxwxxwwwx",
+	"xwwwwwwwwwwwwwwwwwwwwwwwwx",
+	"xwwxxwxxwxxwxxwxxwxxwxxwwx",
+	"xwwwwwwwwwwwwwwwwwwwwwwwwx",    //20
+	"xwwwxxwxxwxxwxxwxxwxxwxxwx",
+	"xwwwwwwwwwwwwwwwwwwwwwwwwx",
+	"xwwxxwxxwxxwxxwxxwxxwxxwwx",
+	"xwwwwwwwwwwwwwwwwwwwwwwwwx",
+	"xxxxxxxxxxxxxxxxxxxxxxxxxx" 	 //25
 };
 
 char *bitmap_button_ext_desktop_my_machine_cz2000[EXT_DESKTOP_BUTTONS_ANCHO]={
