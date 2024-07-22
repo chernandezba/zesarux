a3d49e8b491197781f6aaf832aa2e05ed05f34ab
chernandezba <chernandezba@hotmail.com>
2024-07-22 23:33:27 +0200

    improve memory cheat window

--- src/menu/menu_items.h
+++ src/menu/menu_items.h
@@ -179,6 +179,8 @@ extern void menu_ql_mdv_flp(MENU_ITEM_PARAMETERS);
 
 extern void menu_debug_unnamed_console(MENU_ITEM_PARAMETERS);
 
+extern void menu_memory_cheat(MENU_ITEM_PARAMETERS);
+
 extern void menu_audio_general_sound(MENU_ITEM_PARAMETERS);
 
 extern void menu_debug_ioports(MENU_ITEM_PARAMETERS);
--- src/zxvision.c
+++ src/zxvision.c
@@ -9622,6 +9622,7 @@ zxvision_known_window_names zxvision_known_window_names_array[]={
     {"helpshowkeyboard",    "helpkeybrd",   menu_help_show_keyboard,bitmap_button_ext_desktop_helpkeyboard},
     {"hexeditor",           "hex editor",   menu_debug_hexdump,bitmap_button_ext_desktop_hexeditor},
     {"hilowconvertaudio",   "hlw cnv au",   menu_hilow_convert_audio,bitmap_lowericon_ext_desktop_hilow_convert},
+    {"memorycheat",         "memcheat",     menu_memory_cheat,bitmap_button_ext_desktop_userdefined},
     {"networktraffic",      "nettraffic",   menu_network_traffic,bitmap_button_ext_desktop_networktrafiic},
     {"processmanagement",   "prc mngmnt",   menu_display_window_list,bitmap_button_ext_desktop_processmanagement},
     {"processswitcher",     "proc swtch",   menu_process_switcher,bitmap_button_ext_desktop_processswitcher},
