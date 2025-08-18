e0e92b3b6f63d7f592e80ea90c6bb620defaa19b
chernandezba <chernandezba@hotmail.com>
2025-08-18 18:05:46 +0200

    initial zx81 enhanced read

---
+++ src/enhanced_zx81_read.c
@@ -0,0 +1,39 @@
+/*
+    ZEsarUX  ZX Second-Emulator And Released for UniX
+    Copyright (C) 2013 Cesar Hernandez Bano
+
+    This file is part of ZEsarUX.
+
+    ZEsarUX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#include "cpu.h"
+
+//tipos de datos que luego se obtendrán desde cpu.h
+//64 bits
+//typedef long long int z80_64bit;
+
+int main(void)
+{
+
+
+
+    return 0;
+}
