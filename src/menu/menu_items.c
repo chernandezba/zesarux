ed0bc8552ba3180d74778ff8ef8849ce64fea3a3
chernandezba <chernandezba@hotmail.com>
2026-02-02 00:44:39 +0100

    mc

--- src/menu/menu_items.c
+++ src/menu/menu_items.c
@@ -32322,7 +32322,7 @@ void menu_about_statistics(MENU_ITEM_PARAMETERS)
 
         "\n"
         "Edited with Visual Studio Code and vim\n"
-        "Developed on macOS Catalina, Debian 12, Raspbian, FreeBSD 12, and MinGW environment on Windows\n"
+        "Developed on macOS Catalina and Tahoe, Debian 12, Raspbian, FreeBSD 12, and MinGW environment on Windows\n"
         ,
         LINES_SOURCE,
         TOTAL_COMMENTS,
