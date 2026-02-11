bde82b9519121e0b347a80914f3c4ad0bdefde19
chernandezba <chernandezba@hotmail.com>
2026-02-11 23:39:38 +0100

    mc

--- src/cpu.h
+++ src/cpu.h
@@ -54,8 +54,8 @@ Examples
 //Para obtener EMULATOR_NUMBER_VERSION se deberia usar siempre util_get_emulator_version_number
 #define EMULATOR_NUMBER_VERSION EMULATOR_VERSION
 
-#define EMULATOR_DATE "2 February 2026"
-#define EMULATOR_SHORT_DATE "02/02/2026"
+#define EMULATOR_DATE "11 February 2026"
+#define EMULATOR_SHORT_DATE "11/02/2026"
 #define EMULATOR_GAME_EDITION "xxx"
 #define EMULATOR_EDITION_NAME EMULATOR_GAME_EDITION " edition"
 #define ZESARUX_EXTRAS_HOST "github.com"
