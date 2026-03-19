e32f53dd96c46ba6d6610a50616b9e7fba436cee
chernandezba <chernandezba@hotmail.com>
2026-03-19 17:52:10 +0100

    Allow to reduce display on Spectrum, TSConf, Prism, ZX80, ZX81, Z88, Colecovision, MSX, SG1000, Master System, Spectravideo

--- src/video/screen.c
+++ src/video/screen.c
@@ -5284,6 +5284,14 @@ void scr_refresca_pantalla_rainbow_comun(void)
 
     puntero=rainbow_buffer;
 
+	//Si se reduce la pantalla 0.75
+	if (screen_reduction_factor!=SCREEN_REDUCE_NONE) {
+		screen_scale_075_050_025_function(ancho,alto);
+		puntero=new_scalled_rainbow_buffer;
+	}
+	//Fin reduccion pantalla 0.75
+
+
     for (y=0;y<alto;y++) {
         for (x=0;x<ancho;x++) {
             color_pixel=*puntero++;
