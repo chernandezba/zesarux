4a39e7e3c992ce805f884b7bca9bc50d183c289e
chernandezba <chernandezba@hotmail.com>
2026-03-26 22:49:32 +0100

    mc

--- src/menu/menu_items_settings.c
+++ src/menu/menu_items_settings.c
@@ -1999,15 +1999,19 @@ void menu_main_window_special_effects(MENU_ITEM_PARAMETERS)
 
                 if (type==SCREEN_EFFECT_TYPE_BLUR && enabled) {
 
-                    menu_add_item_menu_en_es_ca(array_menu_common,MENU_OPCION_NORMAL,menu_interface_special_effects_blur_intensity,NULL,
-                        "Blur internsity","Intensidad desenfoque","Intensitat desenfocament");
-                    menu_add_item_menu_prefijo_format(array_menu_common," [%d] ",screen_rainbow_effect_blur_intensity);
-
                     menu_add_item_menu_en_es_ca(array_menu_common,MENU_OPCION_NORMAL,NULL,NULL,
                         "Follow Mouse","Sigue Ratón","Segueix Ratolí");
                     menu_add_item_menu_prefijo_format(array_menu_common," [%c] ",(screen_rainbow_effect_blur_follow_mouse.v ? 'X' : ' ' ));
                     menu_add_item_menu_opcion_conmuta(array_menu_common,&screen_rainbow_effect_blur_follow_mouse);
 
+
+                    if (screen_rainbow_effect_blur_follow_mouse.v==0) {
+
+                        menu_add_item_menu_en_es_ca(array_menu_common,MENU_OPCION_NORMAL,menu_interface_special_effects_blur_intensity,NULL,
+                            "Blur intensity","Intensidad desenfoque","Intensitat desenfocament");
+                        menu_add_item_menu_prefijo_format(array_menu_common," [%d] ",screen_rainbow_effect_blur_intensity);
+
+                    }
 
                 }
 
