466035f6aa91b6b2ebec1e7ee20c546303f475f3
chernandezba <chernandezba@hotmail.com>
2026-07-22 23:53:27 +0200

    Fixed pressing redefined top button with extra parameters

--- src/zxvision/zxvision.c
+++ src/zxvision/zxvision.c
@@ -8588,7 +8588,7 @@ void tooltips_mouse_timer_event_which_element(struct s_tooltip_mouse *tooltip)
         tooltip->texto_tooltip=menu_inicio_retorna_tooltip(id_tooltip);
 
         //Boton esta definido por el usuario?
-        int indice_tabla=menu_inicio_return_button_userdef(boton);
+        int indice_tabla=menu_inicio_return_button_userdef(boton-1);
         if (indice_tabla>=0) {
             //printf("user defined button\n");
             //En ese caso el texto del tooltip simplemente es el texto de la funcion definida
@@ -28984,8 +28984,6 @@ void menu_inicio_handle_configurable_icon_presses(void)
 //Retorna <0 si no esta. >=0 si esta redefinido, y retorna indice a tabla a la funcion mapeada
 int menu_inicio_return_button_userdef(int boton)
 {
-    boton--;
-
     //El 0 no esta permitido
     if (boton<0 || boton>=MAX_USERDEF_BUTTONS) return -1;
 
@@ -29007,6 +29005,8 @@ int menu_inicio_return_button_userdef(int boton)
 //Mira si el boton pulsado esta redefinido por el usuario y lanza accion si conviene
 int menu_inicio_handle_button_presses_userdef(int boton)
 {
+    boton--;
+
     int indice_tabla=menu_inicio_return_button_userdef(boton);
     if (indice_tabla<0) return 0;
 
@@ -29501,7 +29501,7 @@ void menu_process_f_functions_by_action_index(int indice,int si_pulsado_boton_re
 
     int id_funcion=menu_da_accion_direct_functions_indice(indice);
 
-    menu_process_f_functions_by_action_name(id_funcion,0,menu_button_f_function_index,si_pulsado_boton_redefinido,boton-1,0,0);
+    menu_process_f_functions_by_action_name(id_funcion,0,menu_button_f_function_index,si_pulsado_boton_redefinido,boton,0,0);
 }
 
 void menu_process_f_functions(void)
